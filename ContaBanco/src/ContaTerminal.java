b6780bd7191f88c531f0a6a3ce83043757e9b2aa
gustavo.ribeiro <gustavo.ribeiro@sc.senai.br>
2023-08-12 12:23:24 -0300

    Conta Banco finalizada

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o número da conta: ");
+        int numero = Integer.parseInt(sc.nextLine());
+        System.out.println("Digite o número da agencia: ");
+        String agencia = String.valueOf(sc.nextLine());
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente = String.valueOf(sc.nextLine());
+        System.out.println("Digite o saldo: ");
+        Double saldo = Double.parseDouble(sc.nextLine());
+
+        System.out.println("Olá " + nomeCliente
+                + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + numero
+                + " e seu saldo " + saldo
+                + " já está disponível para saque!");
     }
 }
